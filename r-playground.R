3a997e394af10a6afb994bcd3d25b4907fa233b5
Dennis Mecha <dennismecha4@gmail.com>
2025-09-30 12:01:40 +0300

    Lesson on histograms

--- r-playground.R
+++ r-playground.R
@@ -3,15 +3,15 @@ require(pacman)
 library(datasets)
 head(iris)
 
-# plots
+# PLOTS
 plot(iris$Species)
 plot(iris$Petal.Length, iris$Petal.Width)
 
-## plot formulas
+## Plot formulas
 plot(cos, 0, 2*pi)
 plot(tan, 0, 2*pi)
 
-# Bar graphs
+# BAR GRAPHS
 ?mtcars
 head(mtcars)
 
@@ -24,3 +24,53 @@ plot(cylinders) # DEfault X-Y plot (lines)
 
 # Clear environment
 rm(list = ls())
+
+# HISTOGRAMS
+
+# Load Packages ####
+library(datasets)
+
+# Load Data ####
+?iris
+head(iris)
+
+## Basic Histogram
+hist(iris$Sepal.Length)
+hist(iris$Sepal.Width)
+hist(iris$Petal.Length)
+hist(iris$Petal.Width)
+
+## Histogram by Group
+
+# Put graphs in 3 rows and 1 col
+par (mfrow = c(3,1))
+
+# Histogram for each species using options
+hist(iris$Petal.Width [iris$Species == "setosa"],
+     xlim = c(0,3),
+     breaks =9,
+     main = "Petal Width for Setosa",
+     xlab = "",
+     col = "red")
+
+hist(iris$Petal.Width [iris$Species == "versicolor"],
+     xlim = c(0,3),
+     breaks =9,
+     main = "Petal Width for Versicolor",
+     xlab = "",
+     col = "purple")
+
+hist(iris$Petal.Width [iris$Species == "virginica"],
+     xlim = c(0,3),
+     breaks =9,
+     main = "Petal Width for Virginica",
+     xlab = "",
+     col = "green")
+
+# Restore graphic parameter
+par(mfrow=c(1, 1))
+
+# CLEAN UP ####
+
+# Clear Packages
+detach ("package:datasets", unload = TRUE) # For base
