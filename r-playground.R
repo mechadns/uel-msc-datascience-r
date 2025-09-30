48f827c262e2688c3c04f83ea257bfb6e1a105cc
Dennis Mecha <dennismecha4@gmail.com>
2025-09-30 11:38:04 +0300

    Lesson on Bar Graphs

--- r-playground.R
+++ r-playground.R
@@ -2,11 +2,25 @@ install.packages("pacman")
 require(pacman)
 library(datasets)
 head(iris)
+
+# plots
 plot(iris$Species)
 plot(iris$Petal.Length, iris$Petal.Width)
 
-
+## plot formulas
 plot(cos, 0, 2*pi)
-
 plot(tan, 0, 2*pi)
 
+# Bar graphs
+?mtcars
+head(mtcars)
+
+## Create a summary table
+cylinders <- table(mtcars$cyl) # Create table
+barplot(cylinders) # Bar chart
+plot(cylinders) # DEfault X-Y plot (lines)
+
+# CLEANUP ####
+
+# Clear environment
+rm(list = ls())
