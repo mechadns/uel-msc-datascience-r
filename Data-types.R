eea08bf1d0932ed1ea9f98212f8b9826d251aec4
Dennis Mecha <dennismecha4@gmail.com>
2025-10-06 10:04:08 -0400

    Data types lesson

---
+++ Data-types.R
@@ -0,0 +1,22 @@
+# DATA TYPES ####
+
+# Numeric
+
+n1 <- 15 # Double precision by default
+n1
+typeof(n1)
+
+n2 <- 1.5
+n2
+typeof(n2)
+
+# Character
+c1 <- "c"
+c
+typeof(c1)
+
+# Logical
+
+l1 <- TRUE
+l1
+typeof(l1)
