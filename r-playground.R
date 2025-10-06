39becd7ee248ef34dda9ef527a0147f3ce4b3f3d
Dennis Mecha <dennismecha4@gmail.com>
2025-10-06 09:48:01 -0400

    Basic statistics in R lesson

--- r-playground.R
+++ r-playground.R
@@ -133,3 +133,87 @@ curve(dnorm(x, mean = mean(lynx), sd = sd(lynx)),
       lwd = 2, # Line width of 2 pixels
       add = TRUE) # Superimpose on previous graph
 
+# CLEAN UP ####
+
+# Clear Packages
+detach ("package:datasets", unload = TRUE) # For base
+
+# BASIC STATISTICS #
+
+# Load Packages ####
+library(datasets)
+
+# Load Data ####
+head(iris)
+
+## SUMMARY #
+
+summary(iris$Species) # Catergorical variable
+summary(iris$Sepal.Length) #Quantitative variable
+summary(iris) #Entire data frame
+
+## DESCRIBE #
+
+# Install and Load Packages ####
+pacman::p_load(pacman, dplyr, GGally, ggplot2, ggthemes,
+               ggvis, httr, lubridate, plotly, rio, rmarkdown, shiny,
+               stringr, tidyr)
+pacman::p_load(dplyr)
+
+# Psych Package ####
+
+# Load psych package
+p_load(psych)
+
+# Get info on package
+p_help(psych) # Opens package PDF in browser
+p_help(psych, web = F) # Opens help in R viewer
+
+# Describe ####
+
+# For quantitative variables only.
+
+describe(iris$Sepal.Length) # One quantitative variable
+describe(iris) # Entire data frame
+
+
+# SELECTING CASES ####
+
+hist(iris$Petal.Length)
+summary(iris$Petal.Length)
+
+summary(iris$Species)
+
+## Select cases by category ####
+
+# Versicolor
+hist(iris$Petal.Length[iris$Species == "versicolor"],
+     main = "Petal Length: Versicolor")
+
+# Virginica
+hist(iris$Petal.Length[iris$Species == "virginica"],
+     main = "Petal Length: Virginica")
+
+## Select by value ####
+
+# Short petals only (all Setosa)
+hist(iris$Petal.Length[iris$Petal.Length < 2],
+     main = "Petal Length < 2")
+
+## Multiple selectors ####
+
+# Short Virginica petals only
+hist(iris$Petal.Length[iris$Species == "virginica" & iris$Petal.Length < 5.5],
+     main = "Petal Length: Short Virginica")
+
+# Create Subsample ####
+
+# Format: data[rows, columns]
+# Leave rows or columns blanks to select all
+i.setosa <- iris[iris$Species == "setosa", ]
+
+# Explore SubSample ####
+
+head(i.setosa)
+summary(i.setosa)
+hist(i.setosa$Petal.Length)
